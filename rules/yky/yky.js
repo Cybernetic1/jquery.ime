3c0da9339cce73082bc82d10b772c5c1633d5cb0
Cybernetic1 <generic.intelligence@gmail.com>
2013-11-17 15:34:48 +0800

    added YKY language file

---
+++ rules/yky/yky.js
@@ -0,0 +1,18 @@
+( function ( $ ) {
+	'use strict';
+
+	var cn = {
+		id: 'yky',
+		name: 'YKY shorthands',
+		description: 'YKY`s shorthands, for testing only.',
+		date: '2013-11-17',
+		URL: 'http://github.com/Cybernetic1/jquery.ime',
+		author: 'YKY',
+		license: 'GPLv3',
+		version: '1.0',
+		patterns: [
+			['cyb1', '']]
+	};
+
+	$.ime.register( cn );
+}( jQuery ) );
